e132def0209d8a56f64fc76f7411dbec5c577bf1
crywolfe <gwolfe2007@gmail.com>
2014-03-28 15:46:59 -0400

    set up rails and initial user MVC

---
+++ taba/app/controllers/session_controller.rb
@@ -0,0 +1,17 @@
+class SessionController < ApplicationController
+
+	def new
+	end
+
+	def create
+	end
+
+	def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+end
---
+++ taba/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Taba::Application.config.secret_key_base = '9a18d7a7feb19390f04bae84b370fc7a73ad7bd9fa0e67676c76333131bdc8c250f95b92b89230e5dc5813774367f1d563d38fe75c66acec895e49e05fc53a9f'
