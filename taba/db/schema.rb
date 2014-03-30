fa663ef0002ccd7d55dc07e2728703c12fa0db4f
crywolfe <gwolfe2007@gmail.com>
2014-03-30 12:15:56 -0400

    create add_admin_column_to_users table

--- taba/db/schema.rb
+++ taba/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140329165129) do
+ActiveRecord::Schema.define(version: 20140330161236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20140329165129) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",           default: false
   end
 
 end
