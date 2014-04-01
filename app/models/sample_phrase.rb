4136f58052d7c11b884e65e8801bcc66a7c0f73e
crywolfe <gwolfe2007@gmail.com>
2014-04-01 11:23:31 -0400

    working up delimiter reg ex

--- app/models/sample_phrase.rb
+++ app/models/sample_phrase.rb
@@ -39,30 +39,31 @@ class SamplePhrase < ActiveRecord::Base
 	def self.extract(journal_entry)
 
 		#WILL DEFINITELY NEED TO REFACTOR THIS...
-
+		# journal_entry = "This is a sentence.  I am feeling great!  What do you think?  Yeah :)  Next time."
 		#start with ! as one of many punctuations
-		delimiter = "! "
+		delimiter = /(\. |\.|! |\? |\...)/
 		#split on the delimiter
 		phrase = journal_entry.split(delimiter)
 		# handle phrase if array elements are greater than one
 		if phrase.size > 1
 			phrase.each do |n|
-				phrase[phrase.index(n)] += "!"
+				#need length limitation
+				# s = phrase.index(n) + 1
+				phrase[phrase.index(n)] << phrase[phrase.index(n)+1]
+				phrase.slice!(phrase.index(n)+1)
+				# phrase[phrase.index(n)] += delimiter
 			end
 
-			# take out the additional delimiter on the last array element.
-			phrase_chars = phrase[-1].chars
-			phrase_chars.pop
-			rejoined_phrase = phrase_chars.join
-			phrase[-1] = rejoined_phrase
+			# # take out the additional delimiter on the last array element.
+			# phrase_chars = phrase[-1].chars
+			# phrase_chars.pop
+			# rejoined_phrase = phrase_chars.join
+			# phrase[-1] = rejoined_phrase
 		end
 
 		#return array of phrases on method call
 		phrase
 	end
-
-	
-
 
 
 end
