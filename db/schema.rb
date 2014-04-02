02eb63c778865305d39ab3bac8bc44b315d2d6c5
crywolfe <gwolfe2007@gmail.com>
2014-04-01 23:01:50 -0400

    reseeded file, changed column datatype from string to text for body in journal_entries

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140331192751) do
+ActiveRecord::Schema.define(version: 20140402025617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "journal_entries", force: true do |t|
-    t.string   "body"
+    t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
