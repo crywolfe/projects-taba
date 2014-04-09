93f5e65965767ff351888fa7fa0a68a93fd4a198
crywolfe <gwolfe2007@gmail.com>
2014-04-09 07:37:19 -0400

    added some CSS.  added application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+	def oauth
+
+		"https://accounts.google.com/o/oauth2/auth?scope=https://www.googleapis.com/auth/devstorage.read_write&redirect_uri=<%=ENV['GOOGLE_REDIRECT_URIS']%>&response_type=code&client_id=<%=ENV['GOOGLE_CLIENT_ID']"	
+	end
 end
