507b2ce291384a76502d622709d249734901165c
crywolfe <gwolfe2007@gmail.com>
2014-04-09 23:36:01 -0400

    used rubocop, did some cleaning

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,49 +7,49 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 new_user = User.create(
-	name: "Gerald",
-	email: "gwolfe2007@gmail.com",
-	password: "12345678",
-	password_confirmation: "12345678",
-	admin: "t"
+	name: 'Gerald',
+	email: 'gwolfe2007@gmail.com',
+	password: '12345678',
+	password_confirmation: '12345678',
+	admin: 't'
 
 	)
 
 new_user2 = User.create(
-	name: "John",
-	email: "john@gmail.com",
-	password: "12345678",
-	password_confirmation: "12345678",
-	admin: "f"
+	name: 'John',
+	email: 'john@gmail.com',
+	password: '12345678',
+	password_confirmation: '12345678',
+	admin: 'f'
 	)
 
 new_journal_entry = JournalEntry.create(
-	body: "I had such a wonderful time today!  LOL alot.  Great conversation with my wife.  I spent my whole day programming.\n  My lunch was ok, but dinner at Chipotle was +1!  Overall, today was above average.  The weather was decent.  I went to the top of a building to look at the view.  Wow!  The Empire State Building is quite fantastic.  I Love NYC!  Who wouldn't?  Great workout today too.  Another train ride, another day.  \n  Again?",
-	user_id: "1"
+	body: 'I had such a wonderful time today!  LOL alot.  Great conversation with my wife.  I spent my whole day programming.\n  My lunch was ok, but dinner at Chipotle was +1!  Overall, today was above average.  The weather was decent.  I went to the top of a building to look at the view.  Wow!  The Empire State Building is quite fantastic.  I Love NYC!  Who wouldn't?  Great workout today too.  Another train ride, another day.  \n  Again?',
+	user_id: '1'
 	)
 
 new_journal_entry2 = JournalEntry.create(
-	body: "My day was awful.  It rained, rained and rained.\n  :(  \n What am I going to do?  Dinner was even worse!  The only saving grace was watching the Flatiron building.  So many people don't realize the significance of this building.  Just love it!  What would I rather have good weather and no Flatiron or inclement weather and Flatiron?\n  I should watch a movie. How about Lord of the Rings?  Love Frodo's book, 'There and Back Again'.",
-	user_id: "2"
+	body: 'My day was awful.  It rained, rained and rained.\n  :(  \n What am I going to do?  Dinner was even worse!  The only saving grace was watching the Flatiron building.  So many people don't realize the significance of this building.  Just love it!  What would I rather have good weather and no Flatiron or inclement weather and Flatiron?\n  I should watch a movie. How about Lord of the Rings?  Love Frodo's book, 'There and Back Again'.',
+	user_id: '2'
 	)
 
 new_journal_entry3 = JournalEntry.create(
-	body: "Cool.  Cool!  COOL!  That is all I can say about today.",
-	user_id: "1"
+	body: 'Cool.  Cool!  COOL!  That is all I can say about today.',
+	user_id: '1'
 	)
 ###  Seed Moods Table with lots of data ###
 
-moods_array = ["","Aggravated",
-	"Alone",
-	"Amused",
-	"Angry",
-	"Annoyed",
-	"Excited",
-	"Happy",
-	"Relaxed",
-	"Rejuvenated",
-	"Surprised",
-	"Thankful"]
+moods_array = ['','Aggravated',
+	'Alone',
+	'Amused',
+	'Angry',
+	'Annoyed',
+	'Excited',
+	'Happy',
+	'Relaxed',
+	'Rejuvenated',
+	'Surprised',
+	'Thankful']
 
 moods_array.each do |mood|
 	Mood.create(
--- spec/models/sample_phrase_spec.rb
+++ spec/models/sample_phrase_spec.rb
@@ -1,58 +1,44 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe JournalEntry do
 
-	# one, two and three sentences with one punctuation "!"
-	describe "#extract" do
-		
-		it "should return a 3 element array from a three sentence string" do
-			entry = JournalEntry.new
-			entry.body = "What a great day! I had a great time! Simply wonderful!"
-			output = ["What a great day! ", "I had a great time! ", "Simply wonderful!"]
-
-			sentences = entry.extract_sentences
-
-			expect(sentences).to eq(output)
+	# one, two and three sentences with one punctuation '!'
+	describe '#extract' do
+		it 'should return a 3 element array from a three sentence string' do
+      entry = JournalEntry.new
+      entry.body = 'What a great day! I had a great time! Simply wonderful!'
+      output = ['What a great day! ', 'I had a great time! ', 'Simply wonderful!']
+      sentences = entry.extract_sentences
+      expect(sentences).to eq(output)
 		end		
 
-		it "should return a 2 element array from a two sentence string" do
+		it 'should return a 2 element array from a two sentence string' do
 			entry = JournalEntry.new
-			entry.body = "What a great day! I had a great time!"
-			
-			output = ["What a great day! ", "I had a great time!"]
-
-			sentences = entry.extract_sentences
+			entry.body = 'What a great day! I had a great time!'
+			output = ['What a great day! ', 'I had a great time!']
+      sentences = entry.extract_sentences
 
 			expect(sentences).to eq(output)
 		end
 
-		it "should return a 1 element array from a single sentence string" do
+		it 'should return a 1 element array from a single sentence string' do
 			entry = JournalEntry.new
-			entry.body = "What a great day!"
+			entry.body = 'What a great day!'
 
-			output = ["What a great day!"]
+			output = ['What a great day!']
 
 			sentences = entry.extract_sentences
-
-			expect(sentences).to eq(output)
+      expect(sentences).to eq(output)
 
 		end
 
 		# two sentences with two punctuations
-		it "should return a 2 element array from a two sentence string with two punctuations" do
+		it 'should return a 2 element array from 2 sentences with 2 punctuations' do
 			entry = JournalEntry.new
-			entry.body = "What a great day? Did I have a great time!"
-			output = ["What a great day? ", "Did I have a great time!"]
+			entry.body = 'What a great day? Did I have a great time!'
+			output = ['What a great day? ', 'Did I have a great time!']
 			sentences = entry.extract_sentences
-
-			expect(sentences).to eq(output)
-		end
-
+      expect(sentences).to eq(output)
+    end
 	end
-
-
-	
-
-
-
 end
